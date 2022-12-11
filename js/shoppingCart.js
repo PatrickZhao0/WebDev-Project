12bc84e3f9820912e285e30262f5a2cd23d6f119
Patrick Zhao <bz2099@nyu.edu>
2022-12-11 05:06:02 -0500

    Patrick: Dec 11

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,22 +1,27 @@
 let quantities = document.querySelectorAll(".product_qauntity");
 let unitPrices = document.querySelectorAll(".unitPrice");
-let subtotals = document.querySelectorALl(".subtotal");
+let subtotals = document.querySelectorAll(".subtotal");
 let total = document.querySelector(".total");
 let inputs = document.forms[0].elements;
 let tel = inputs[3];
 let email = inputs[2];
 let zip = inputs[7];
 
-for(let quantity of quantities){
-    quantity.addEventListener("change", total);
-}
+console.log(document.forms[0]);
 
 document.forms[0].addEventListener("submit", function(e){
     e.preventDefault();
     validate();
 });
 
-function total(){
+for(let quantity of quantities){
+    console.log(quantity);
+    quantity.addEventListener("change", calcTotal);
+}
+
+
+
+function calcTotal(){
     let tempTotal = 0;
     for(let item = 0; item < quantities.length; item ++){
         subtotals[item].value = quantities[item].value * unitPrices[item].value;
@@ -32,11 +37,20 @@ function validate(){
             input.focus();
             input.style.backgroundColor = "#CBCBCB";
             alert("Fields are Empty")
-            break;
+            empty = true;
         }
-        
-        if (tel.value.length != 9) alert("Phone number should be 9 digits");
-        if (zip.value.length != 5) alert("Zip code should be 5 digit");
-        if (email.value.indexOf(".") == -1 || email.value.indexOf("@") == -1) alert("Your email address is missing '@' or '.'");
+        if (empty) return;
+        if (tel.value.length != 9){
+            alert("Phone number should be 9 digits");
+            return;
+        }
+        if (zip.value.length != 5){
+            alert("Zip code should be 5 digit");
+            return;
+        }
+        if (email.value.indexOf(".") == -1 || email.value.indexOf("@") == -1){
+            alert("Your email address is missing '@' or '.'");
+            return;
+        }
     }
 }

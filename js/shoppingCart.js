d9a752cc5bac91aec98283db6e969e8e0ade07fb
Patrick Zhao <bz2099@nyu.edu>
2022-12-11 04:49:41 -0500

    Patrick: Dec 11 04:49

---
+++ js/shoppingCart.js
@@ -0,0 +1,42 @@
+let quantities = document.querySelectorAll(".product_qauntity");
+let unitPrices = document.querySelectorAll(".unitPrice");
+let subtotals = document.querySelectorALl(".subtotal");
+let total = document.querySelector(".total");
+let inputs = document.forms[0].elements;
+let tel = inputs[3];
+let email = inputs[2];
+let zip = inputs[7];
+
+for(let quantity of quantities){
+    quantity.addEventListener("change", total);
+}
+
+document.forms[0].addEventListener("submit", function(e){
+    e.preventDefault();
+    validate();
+});
+
+function total(){
+    let tempTotal = 0;
+    for(let item = 0; item < quantities.length; item ++){
+        subtotals[item].value = quantities[item].value * unitPrices[item].value;
+        tempTotal += subtotals[item].value;
+    }
+    total.value = tempTotal;
+}
+
+function validate(){
+    let empty = false;
+    for(let input of inputs){
+        if (input.value == "" || input.value == null){
+            input.focus();
+            input.style.backgroundColor = "#CBCBCB";
+            alert("Fields are Empty")
+            break;
+        }
+        
+        if (tel.value.length != 9) alert("Phone number should be 9 digits");
+        if (zip.value.length != 5) alert("Zip code should be 5 digit");
+        if (email.value.indexOf(".") == -1 || email.value.indexOf("@") == -1) alert("Your email address is missing '@' or '.'");
+    }
+}

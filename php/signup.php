3deb11105aeb3fc6d263f95fab4c2bff2e6e2994
Patrick Zhao <bz2099@nyu.edu>
2022-12-12 11:11:40 -0500

    Patrick: Dec 12 11:11

--- php/signup.php
+++ php/signup.php
@@ -12,8 +12,19 @@
         <!--End Fonts-->
 
         <!--CSS File-->
-        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="../css/style.css" />
         <!--End CSS-->
+
+        <style>
+            footer {
+            position: fixed;
+            left: 0;
+            bottom: 10px;
+            width: 100%;
+            text-align: center;
+            }
+        </style>
+
     </head>
 
     <body>
@@ -28,9 +39,26 @@
                 </ul>
             </nav>
         </div>
+        <h1>
+            Thank You For Your Registration!
+        </h1>
+        <h1 style="font-size: 10vw; margin-top: 5vh; margin-bottom: 5vh;">✅</h1>
+
+        <a href="../index.html" style="display: flex; justify-content: center;"><button class="button">Back To Login In</button></a>
+        
         <?php
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $line = $username . ":" . $password . ";" . $email . "\n";
 
+            $file = fopen("../data/user.text", "a");
 
+            fwrite($file, $line);
+            fclose($file);
         ?>
+
+        <footer style="text-align: center; margin-top: 10px;">Copyright &copy 2022 Emoji Mayhem. All right reserved.</footer>
+
     </body>
     

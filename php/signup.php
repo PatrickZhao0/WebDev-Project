0c85281a0704e4d8f00baaed4a7b8ae95c2c027e
Patrick Zhao <bz2099@nyu.edu>
2022-12-11 20:18:01 -0500

    Patrick: Idea added

---
+++ php/signup.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Home</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="initial-scale=1" />
+
+        <!--Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Russo One" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Hind" rel="stylesheet" />
+        <!--End Fonts-->
+
+        <!--CSS File-->
+        <link rel="stylesheet" href="css/style.css" />
+        <!--End CSS-->
+    </head>
+
+    <body>
+        <div>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="start_game.html">Play 🕹️ </a></li>
+                <li><a href="search.html">Search Emoji 🔎 </a></li>
+                <li><a href="shopping_cart.html">Shopping Cart 🛒 </a></li>
+                    <li><a href="contact.html">Contact Us 📞</a></li>
+                    <li><a href="index.html">Log Out 🚪🚶</a></li>
+                </ul>
+            </nav>
+        </div>
+        <?php
+
+
+        ?>
+    </body>
+    
